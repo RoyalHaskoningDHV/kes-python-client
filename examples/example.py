5aac794c8c7e36106b83b83ea6a914e4bb5d7444
twiggler <dejongroel@gmail.com>
2022-03-27 21:59:59 +0200

    doc: add example

---
+++ examples/example.py
@@ -0,0 +1,44 @@
+from library import CategoryAssetRow, Multipleselect, Singleselect
+from kes.client import Client, ClientConfig
+from library import category_asset_table_def
+import os
+
+# Initialize client
+config = ClientConfig(kes_service_address='localhost:50051')
+client = Client(config)
+
+# Lookup project
+project = client.openProject("Preview Python client example")
+
+# Create a table
+activity = project.activities[0]
+table = activity.build_table(category_asset_table_def)
+
+# Read existing rows
+table.load()
+
+# Deleting row if it exists
+if len(table) > 0:
+    del table[0]
+
+# Create a row
+row = CategoryAssetRow(
+    singleselect=Singleselect.A,
+    multipleselect=Multipleselect.D | Multipleselect.F,
+    text="Text",
+    amount=12.0
+)
+
+# Add a location
+row.location.addPoint(address="Meppel - Assen",
+                      latitude=52.77035725849205,
+                      longitude=6.49079267010161,
+                      name="Switch")
+# Add a image
+scriptPath = os.path.realpath(__file__)
+filePath = os.path.join(os.path.dirname(scriptPath), "goofy.png")
+imageFile = open(filePath, "rb")
+table.saveImage(row.image, "Goofy", imageFile.read())
+
+# Add the row
+table.appendRow(row)
